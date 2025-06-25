4e6dec5f79b7cdfc1ca66c585d13b5706c2b3956
Abdurrahman <abdurrahmanpolat453@gmail.com>
2025-06-25 16:26:40 +0300

    👨🏼 Integration App created

---
+++ IntegrationApp/IntegrationApp/IntegrationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegrationAppApp.swift
+//  IntegrationApp
+//
+//  Created by Abdurrahman Polat on 25.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct IntegrationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
